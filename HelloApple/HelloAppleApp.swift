7dcae9dc7aecd1eeef039d7cd27f079114300650
suraiyabd80 <suraiyabd80@gmail.com>
2021-02-20 17:46:30 +0600

    First commit

---
+++ HelloApple/HelloAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppleApp.swift
+//  HelloApple
+//
+//  Created by Suraiya Khanam on 19/2/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
